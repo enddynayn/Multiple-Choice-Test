13d280ebe5d18aacaba9b3551524cc4984002070
Enddy Dumbrique <enddynayn@hotmail.com>
2013-09-03 16:07:46 -0700

    finish model for exam questions and choices

--- app/models/answer_choice.rb
+++ app/models/answer_choice.rb
@@ -2,5 +2,6 @@ class AnswerChoice
   include Mongoid::Document
 
   field :choice_url, type: String
+  field :correct_choice, type: Boolean
   embedded_in :test_bank_question
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ AnswerChoice.destroy_all
 
 question1 = TestBankQuestion.create(
   	question_url: 'https://s3-us-west-1.amazonaws.com/soaquestions/soasample.png',
-  	answer_choices: [AnswerChoice.new(choice_url: '1'),AnswerChoice.new(choice_url:'2'),AnswerChoice.new(choice_url:'3')]
+  	answer_choices: [AnswerChoice.new(choice_url: '1',correct_choice: true),AnswerChoice.new(choice_url:'2'),AnswerChoice.new(choice_url:'3')]
 )
