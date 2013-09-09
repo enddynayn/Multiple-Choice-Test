0796ab199fe7c0c62fa4c40815a62e0c0fde8ac8
Enddy Dumbrique <enddynayn@hotmail.com>
2013-09-08 18:43:55 -0700

    show exam results complete

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -3,8 +3,13 @@ class ExamsController < ApplicationController
   end
 
   def index
+  	
   end
 
   def show
+  	 @questions = Exam.find(params[:id]).test_bank_question_ids
+  	 exam = Exam.find(params[:id])
+  	 @marked = exam.marked
+  	 @useranswers = Exam.find(params[:id]).user_answer
   end
 end
---
+++ app/models/checkinganswers.rb
@@ -0,0 +1,33 @@
+class Checkinganswers 
+include Mongoid::Document
+
+	# @useranswers = Exam.find(params[:id]).user_answer
+
+	# i = 0 
+	# while (i < @useranswers.length)
+	# 	@useranswers[i]
+	# 	question = Exam.find(params[:id]).test_bank_question_ids[i]
+	 
+	#  	j = 0
+	#  	while (j < 4)
+	#  		TestBankQuestion.find(question).answer_choices[i].choice_url
+	#  		if  TestBankQuestion.find(question).answer_choices[i].id == @useranswers[j]
+	# 			TestBankQuestion.find(question).answer_choices[i].correct_choice
+	# 		end
+	# 	j += 1
+	# 	end
+	# 	i += 1
+	# end
+
+ 	i = 0
+	while( i < Exam.find('5227bfdfe0d1cd12ca00003d').user_answer.length  )
+			answer = Exam.find('5227bfdfe0d1cd12ca00003d').user_answer
+			puts Exam.find('5227bfdfe0d1cd12ca00003d').test_bank_questions[i].answer_choices.find(answer[i]).choice_url
+		i += 1
+		
+	end
+	
+
+end
+
+
