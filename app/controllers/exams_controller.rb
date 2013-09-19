aa13acd16784f77b4bdf0c614ac32a45dcd28806
Enddy Dumbrique <enddynayn@hotmail.com>
2013-09-18 21:27:19 -0700

    Exam controller and views refractored

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,27 +1,19 @@
 class ExamsController < ApplicationController
-  def new
-  end
-
+  
   def create
     exam = current_user.exams.create!(test_bank_questions: TestBankQuestion.all.sample(5))
     redirect_to exam_question_path(exam, '1')
-
-
   end
 
   def index
-  	@exams = current_user.exams
-   
+  	@exams = current_user.exams 
   end
 
   def show
-  	 @questions = Exam.find(params[:id]).test_bank_question_ids
-  	 exam = Exam.find(params[:id])
-  	 @marked = exam.marked
-  	 @useranswers = Exam.find(params[:id]).user_answer
-     puts '*' *100
-     puts @useranswers
-     puts '$' *100
-     @exam = Exam.find(params[:id]).timer
+     current_exam =  Exam.find(params[:id])
+     @test_bank_questions = current_exam.test_bank_questions
+     @user_answers = current_exam.user_answer
+     @marked = current_exam.marked
+  	 @questions = current_exam.test_bank_question_ids
   end
 end
